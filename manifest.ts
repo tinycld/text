1225f809dc7affbe3e6a2da6d7fd974aa5b5980d
Nathan Stitt <nathan@stitt.org>
2026-09-15 11:19:27 -0500

    chore: release v0.3.0

--- manifest.ts
+++ manifest.ts
@@ -1,7 +1,7 @@
 const manifest = {
     name: 'Text',
     slug: 'text',
-    version: '0.2.2',
+    version: '0.3.0',
     description: 'Plain-text and rich-text documents.',
     routes: { directory: 'screens' },
     nav: {
